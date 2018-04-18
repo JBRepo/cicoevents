69c859ad2c745108611da7f1ee645654d3d97549
JBRepo <38049720+JBRepo@users.noreply.github.com>
2018-04-18 13:18:26 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import {Header} from './Event'
-import {  BrowserRouter as Router } from 'react-router-dom';
-
-
-ReactDOM.render(<Router><div><Header/></div></Router>, document.getElementById('root'));
-
